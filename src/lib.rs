d3a86da7f3381ab38146229b9c0c757e2106eef3
Lucky <lucky3028@users.noreply.github.com>
2021-08-08 16:38:52 +0900

    [Add] test

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,20 @@ pub mod domains;
 pub mod globals;
 pub mod listeners;
 pub mod utils;
+
+#[cfg(test)]
+mod test {
+    use crate::domains::redmine;
+    #[tokio::test]
+    async fn fetch_issue() {
+        match redmine::fetch_record_issue(9690).await {
+            Ok(issue) => {
+                // TODO test
+                println!("{:#?}", issue.relations);
+            },
+            Err(err) => {
+                println!("{}", err);
+            }
+        }
+    }
+}
