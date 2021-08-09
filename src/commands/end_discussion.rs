b4f413ba9d58c038822c5b98f8283be3081b878a
Lucky <lucky3028@users.noreply.github.com>
2021-08-09 16:56:22 +0900

    [Fix] useの型

--- src/commands/end_discussion.rs
+++ src/commands/end_discussion.rs
@@ -1,7 +1,8 @@
-use crate::globals::record_id::RecordId;
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::{model::prelude::Message, prelude::Context};
 use std::sync::atomic::Ordering;
+
+use crate::globals::record_id;
 
 // TODO: embed
 
@@ -11,7 +12,7 @@ async fn end_discussion(ctx: &Context, message: &Message) -> CommandResult {
     let cached_record_id = {
         let data_read = ctx.data.read().await;
         data_read
-            .get::<RecordId>()
+            .get::<record_id::RecordId>()
             .expect("Expected RecordId in TypeMap.")
             .clone()
     };
