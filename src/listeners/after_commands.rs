b9b15275b00ba781bf7988712e711340de94ac04
Lucky <lucky3028@users.noreply.github.com>
2021-08-10 19:29:07 +0900

    [Add] コマンドを実行したあとエラーが出ていたら通知する

--- src/listeners/after_commands.rs
+++ src/listeners/after_commands.rs
@@ -1,6 +1,29 @@
-use serenity::{framework::standard::{CommandResult, macros::hook}, model::channel::Message, prelude::Context};
+use serenity::{
+    framework::standard::{macros::hook, CommandResult},
+    model::channel::Message,
+    prelude::Context,
+};
 
 #[hook]
-pub async fn after(ctx: &Context, message: &Message, command_name: &str, command_result: CommandResult) {
-
+pub async fn after(
+    ctx: &Context,
+    message: &Message,
+    command_name: &str,
+    command_result: CommandResult,
+) {
+    if let Err(err) = command_result {
+        let _ = message.reply(&ctx.http, &err).await;
+        if format!("{}", err).contains("fatal") {
+            println!(
+                "[{}] {}の処理中にエラーが発生しました。\nerror: {}\nmessage: {}\nauthor: {} (id: {})\nguild_id: {:?}",
+                message.timestamp,
+                command_name,
+                err,
+                message.content,
+                message.author.name,
+                message.author.id.as_u64(),
+                message.guild_id
+            );
+        }
+    }
 }
