bb5885e0f630b39d0d91cf6502bf0effb19df988
Lucky <lucky3028@users.noreply.github.com>
2021-08-10 22:22:56 +0900

    [Add] AgendaStatuと絵文字、日本語をまとめる

--- src/globals/agendas.rs
+++ src/globals/agendas.rs
@@ -11,6 +11,33 @@ pub enum AgendaStatus {
     Declined,
 }
 
+impl AgendaStatus {
+    pub fn emoji(self) -> char {
+        match self {
+            AgendaStatus::New => '🆕',
+            AgendaStatus::Approved => '⭕',
+            AgendaStatus::Declined => '❌',
+        }
+    }
+
+    pub fn ja(self) -> String {
+        match self {
+            AgendaStatus::New => "新規",
+            AgendaStatus::Approved => "承認",
+            AgendaStatus::Declined => "却下"
+        }.to_string()
+    }
+
+    pub fn from(ch: char) -> Option<AgendaStatus> {
+        match ch {
+            '🆕' => Some(AgendaStatus::New),
+            '⭕' => Some(AgendaStatus::Approved),
+            '❌' => Some(AgendaStatus::Declined),
+            _ => None
+        }
+    }
+}
+
 impl TypeMapKey for Agendas {
     type Value = Arc<RwLock<HashMap<u16, AgendaStatus>>>;
 }
