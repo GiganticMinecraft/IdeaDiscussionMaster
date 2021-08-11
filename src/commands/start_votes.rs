a20474b59a9f0371766a05e03ab7204d47cd9bfa
Lucky <lucky3028@users.noreply.github.com>
2021-08-11 22:25:50 +0900

    [Fix] 誤字修正

--- src/commands/start_votes.rs
+++ src/commands/start_votes.rs
@@ -18,7 +18,7 @@ pub async fn start_votes(ctx: &Context, message: &Message) -> CommandResult {
     let current_agenda_id = current_agenda_id::read(ctx).await;
     let current_agenda_exists = current_agenda_id != 0;
     let description = vec![
-        "提議されている議題についての採決を行います。",
+        "提起されている議題についての採決を行います。",
         "以下のリアクションで投票を行ってください。過半数を超え次第、次の議題へと移ります。",
         ":o:: 承認",
         ":x:: 却下",
