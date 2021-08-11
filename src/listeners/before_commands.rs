546e8f9a5ee0e529b63d5b7afe5ab53613506212
Lucky <lucky3028@users.noreply.github.com>
2021-08-11 18:00:51 +0900

    [Add] デバッグ環境でないとコマンド実行持のログを出さないように

--- src/listeners/before_commands.rs
+++ src/listeners/before_commands.rs
@@ -1,14 +1,20 @@
 use serenity::{framework::standard::macros::hook, model::channel::Message, prelude::Context};
+use std::env;
 
 use crate::globals::record_id;
 
 #[hook]
 pub async fn before(ctx: &Context, message: &Message, command_name: &str) -> bool {
-    println!(
-        "Running command '{}' invoked by '{}'",
-        command_name,
-        message.author.tag()
-    );
+    let is_debug = env::var("IS_DEBUG")
+        .ok()
+        .map(|str| str.parse().unwrap_or(false));
+    if is_debug.is_some() && is_debug.unwrap() {
+        println!(
+            "Running command '{}' invoked by '{}'",
+            command_name,
+            message.author.tag()
+        );
+    }
 
     if message.author.bot {
         return false;
