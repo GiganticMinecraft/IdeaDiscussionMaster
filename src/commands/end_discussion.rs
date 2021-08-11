429483103deabea66142fefb24433aeea88e5565
Lucky <lucky3028@users.noreply.github.com>
2021-08-11 17:28:23 +0900

    [Add] グローバル変数の初期化を会議終了時に行う

--- src/commands/end_discussion.rs
+++ src/commands/end_discussion.rs
@@ -1,7 +1,9 @@
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::{model::prelude::Message, prelude::Context};
 
-use crate::globals::record_id;
+use crate::globals::{
+    agendas, current_agenda_id, record_id, voice_chat_channel_id, voted_message_id,
+};
 
 // TODO: embed
 // TODO: 結果をRedmineとDiscordに送信
@@ -9,7 +11,12 @@ use crate::globals::record_id;
 #[command]
 #[aliases("eid")]
 async fn end_discussion(ctx: &Context, message: &Message) -> CommandResult {
+    agendas::clear(ctx).await;
+    current_agenda_id::clear(ctx).await;
     record_id::clear(ctx).await;
+    voice_chat_channel_id::clear(ctx).await;
+    voted_message_id::clear(ctx).await;
+
     message.reply(ctx, "会議を終了しました。").await?;
 
     Ok(())
