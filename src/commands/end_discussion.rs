780972e7a754f561906b076ca138da0d803d9b29
Lucky <lucky3028@users.noreply.github.com>
2021-08-11 22:30:48 +0900

    [Change] 会議の終了をEmbedで伝える

--- src/commands/end_discussion.rs
+++ src/commands/end_discussion.rs
@@ -1,11 +1,11 @@
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::{model::prelude::Message, prelude::Context};
 
-use crate::globals::{
-    agendas, current_agenda_id, record_id, voice_chat_channel_id, voted_message_id,
+use crate::{
+    domains::{discord_embed, redmine},
+    globals::{agendas, current_agenda_id, record_id, voice_chat_channel_id, voted_message_id},
 };
 
-// TODO: embed
 // TODO: 結果をRedmineとDiscordに送信
 
 #[command]
@@ -13,11 +13,26 @@ use crate::globals::{
 async fn end_discussion(ctx: &Context, message: &Message) -> CommandResult {
     agendas::clear(ctx).await;
     current_agenda_id::clear(ctx).await;
-    record_id::clear(ctx).await;
     voice_chat_channel_id::clear(ctx).await;
     voted_message_id::clear(ctx).await;
 
-    message.reply(ctx, "会議を終了しました。").await?;
+    let record_id = record_id::read(ctx).await;
+    message
+        .channel_id
+        .send_message(&ctx.http, |msg| {
+            msg.embed(|embed| {
+                discord_embed::default_embed(embed, record_id)
+                    .title("会議を終了しました")
+                    .field(
+                        "議事録チケット",
+                        format!("{}{}", redmine::REDMINE_ISSUE_URL, record_id),
+                        false,
+                    )
+            })
+        })
+        .await?;
+
+    record_id::clear(ctx).await;
 
     Ok(())
 }
