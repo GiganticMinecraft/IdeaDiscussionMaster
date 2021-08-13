73c2d40e7e21605fbde039ae35fb1769c6fc112a
Lucky <lucky3028@users.noreply.github.com>
2021-08-13 18:35:28 +0900

    [Fix] コンソールにログを流す条件を修正

--- src/listeners/after_commands.rs
+++ src/listeners/after_commands.rs
@@ -13,7 +13,7 @@ pub async fn after(
 ) {
     if let Err(err) = command_result {
         let _ = message.reply(&ctx.http, &err).await;
-        if format!("{}", err).contains("fatal") {
+        if format!("{}", err).contains("Fatal") {
             println!(
                 "[{}] {}の処理中にエラーが発生しました。\nerror: {}\nmessage: {}\nauthor: {} (id: {})\nguild_id: {:?}",
                 message.timestamp,
