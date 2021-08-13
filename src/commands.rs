ae1a0c8230a76fd4593df8aa78bfd6fdc7840423
Lucky <lucky3028@users.noreply.github.com>
2021-08-14 00:26:58 +0900

    [Add] 追加予定のコマンドをlibに

--- src/commands.rs
+++ src/commands.rs
@@ -1,5 +1,7 @@
-pub mod start_discussion;
+pub mod add_agenda;
+pub mod create_next_record;
 pub mod end_discussion;
-pub mod start_votes;
 pub mod end_votes;
 pub mod help;
+pub mod start_discussion;
+pub mod start_votes;
