b03008c26493396ef87688de22cb1f4a876cb1c8
Lucky <lucky3028@users.noreply.github.com>
2021-08-13 20:46:14 +0900

    [Add] redmineのstructにPartialEqとDefault属性を付与

--- src/domains/redmine.rs
+++ src/domains/redmine.rs
@@ -1,28 +1,28 @@
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default, PartialEq)]
 pub struct RedmineProject {
     pub name: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default, PartialEq)]
 pub struct RedmineIssueTracker {
     pub name: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default, PartialEq)]
 pub struct RedmineIssueStatus {
     pub name: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default, PartialEq)]
 pub struct RedmineIssueRelations {
     pub issue_id: u16,
     pub issue_to_id: u16,
     pub relation_type: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default, PartialEq)]
 pub struct RedmineIssue {
     pub id: u16,
     pub project: RedmineProject,
@@ -34,7 +34,7 @@ pub struct RedmineIssue {
     pub relations: Vec<RedmineIssueRelations>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default, PartialEq)]
 pub struct RedmineIssueResult {
     pub issue: RedmineIssue,
 }
