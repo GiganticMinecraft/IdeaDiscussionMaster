d6455737b4477c81f46d5903c1d90695f67a9ed0
Lucky <lucky3028@users.noreply.github.com>
2021-08-13 20:18:29 +0900

    [Add] 改行

--- src/domains/redmine.rs
+++ src/domains/redmine.rs
@@ -4,20 +4,24 @@ use serde::Deserialize;
 pub struct RedmineProject {
     pub name: String,
 }
+
 #[derive(Debug, Deserialize)]
 pub struct RedmineIssueTracker {
     pub name: String,
 }
+
 #[derive(Debug, Deserialize)]
 pub struct RedmineIssueStatus {
     pub name: String,
 }
+
 #[derive(Debug, Deserialize)]
 pub struct RedmineIssueRelations {
     pub issue_id: u16,
     pub issue_to_id: u16,
     pub relation_type: String,
 }
+
 #[derive(Debug, Deserialize)]
 pub struct RedmineIssue {
     pub id: u16,
@@ -29,6 +33,7 @@ pub struct RedmineIssue {
     #[serde(default)]
     pub relations: Vec<RedmineIssueRelations>,
 }
+
 #[derive(Debug, Deserialize)]
 pub struct RedmineIssueResult {
     pub issue: RedmineIssue,
