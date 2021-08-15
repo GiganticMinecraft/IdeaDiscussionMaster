2b98481fae22fde5f79c89fdc40e63dddeb77cd5
Lucky <lucky3028@users.noreply.github.com>
2021-08-15 15:25:02 +0900

    [Add] 議事録チケットに結果を書き込み、当該チケットを終了する処理

--- src/commands/end_discussion.rs
+++ src/commands/end_discussion.rs
@@ -6,8 +6,19 @@ use serenity::{
 };
 use strum::IntoEnumIterator;
 
+cfg_if::cfg_if! {
+    if #[cfg(test)] {
+        pub use crate::domains::redmine_client::MockRedmineClient as RedmineClient;
+    } else {
+        pub use crate::domains::redmine_client::RedmineClient;
+    }
+}
+
 use crate::{
-    domains::{discord_embed, redmine_api, status::agenda_status},
+    domains::{
+        discord_embed, redmine_api,
+        status::{agenda_status, record_status, trait_status::Status},
+    },
     globals::{agendas, current_agenda_id, record_id, voice_chat_channel_id, voted_message_id},
 };
 
@@ -34,12 +45,16 @@ async fn end_discussion(ctx: &Context, message: &Message) -> CommandResult {
                 .find(|(boolean, _)| *boolean)
                 .map(|(_, group)| group.into_iter().map(|(id, _)| id).collect_vec())
             {
-                array.iter().map(|id| format!("#{}", id)).join(", ")
+                array.iter().map(|id| format!("#{}", id)).collect_vec()
             } else {
-                "-".to_string()
+                vec!["-".to_string()]
             };
-            (state.ja(), issue_ids, false)
+            (state.ja(), issue_ids)
         })
+        .collect_vec();
+    let agendas_result_for_embed = agendas_result
+        .iter()
+        .map(|(state, array)| (state.to_owned(), array.join(", "), false))
         .collect_vec();
     message
         .channel_id
@@ -52,10 +67,27 @@ async fn end_discussion(ctx: &Context, message: &Message) -> CommandResult {
                         format!("{}/issues/{}", redmine_api::REDMINE_URL, record_id),
                         false,
                     )
-                    .fields(agendas_result)
+                    .fields(agendas_result_for_embed)
             })
         })
         .await?;
+
+    let agendas_result = agendas_result
+        .iter()
+        .map(|(status, issue_ids)| {
+            format!("[{}]\n{}\n", status, issue_ids.join(" "))
+        })
+        .collect_vec();
+    let redmine_api = redmine_api::RedmineApi::new(RedmineClient::new());
+    if let Err(err) = redmine_api.add_comments(&record_id, agendas_result).await {
+        return Err(format!("Redmineへのアクセス中にエラーが発生しました。管理者に連絡してください。\nFatalError: {:?}", err).into());
+    }
+    if let Err(err) = redmine_api
+        .update_issue_status(&record_id, &record_status::RecordStatus::Done.id())
+        .await
+    {
+        return Err(format!("Redmineへのアクセス中にエラーが発生しました。管理者に連絡してください。\nFatalError: {:?}", err).into());
+    }
 
     record_id::clear(ctx).await;
     agendas::clear(ctx).await;
