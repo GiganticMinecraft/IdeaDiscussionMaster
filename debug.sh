e5bae5f86e89a172e892dc78a53182eddd0e4dc3
Lucky3028 <lucky3028@users.noreply.github.com>
2021-12-08 14:14:44 +0900

    [Fix] bashスクリプトを使って暗黙的に環境変数を渡す

--- debug.sh
+++ debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source ./.env
-DISCORD_TOKEN=$DISCORD_TOKEN REDMINE_KEY=$REDMINE_KEY EXECUTABLE_ROLE_ID=$EXECUTABLE_ROLE_ID cargo run
+set -a; eval "$(cat .env <(echo) <(declare -x))"; set +a;
+cargo run
