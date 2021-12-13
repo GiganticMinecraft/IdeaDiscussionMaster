326ce62e7fe80584f3f7063de82b91fb7c703b71
Lucky3028 <lucky3028@users.noreply.github.com>
2021-12-13 13:30:39 +0900

    [Fix] GHのトークンをGHAppのものに

--- src/domains/client/github_client.rs
+++ src/domains/client/github_client.rs
@@ -1,3 +1,4 @@
+use create_github_app_token::{errors::Error as CError, publish_token};
 use reqwest::{header, Client};
 use serde_json::json;
 use std::env;
@@ -11,11 +12,23 @@ pub struct GitHubClient {
 
 impl GitHubClient {
     #[allow(clippy::new_without_default)]
-    pub fn new() -> Self {
+    pub async fn new() -> Self {
         GitHubClient {
             reqwest_client: Client::new(),
-            api_key: env::var("GITHUB_KEY").unwrap_or_default(),
+            api_key: Self::create_token().await.unwrap_or_default(),
         }
+    }
+
+    async fn create_token() -> Result<String, CError> {
+        let path = env::var("GH_APP_RSA_KEY_PATH").unwrap_or_default();
+        let app_id = env::var("GH_APP_ID")
+            .ok()
+            .and_then(|str| str.parse::<usize>().ok())
+            .unwrap_or_default();
+
+        Ok(publish_token(app_id, path, "GiganticMinecraft")
+            .await?
+            .token)
     }
 
     pub async fn create_issue(
