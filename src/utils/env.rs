40071cfa99ed55c4f32c9f1e4653f5c129286187
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-11 15:50:39 +0900

    chore(deps): add discordAppId env

--- src/utils/env.rs
+++ src/utils/env.rs
@@ -3,6 +3,7 @@ use std::{env, path::PathBuf};
 
 pub struct Env {
     pub discord_token: String,
+    pub discord_application_id: String,
     pub redmine_api_key: String,
     pub discord_executor_role_id: u64,
     pub github_app_id: u64,
@@ -13,6 +14,8 @@ impl Env {
     pub fn new() -> Self {
         let discord_token =
             env::var("DISCORD_TOKEN").expect("DiscordのBot Tokenが指定されていません");
+        let discord_application_id = env::var("DISCORD_APPLICATION_ID")
+            .expect("DiscordBotのApplication IDが指定されていません");
         let redmine_api_key =
             env::var("REDMINE_KEY").expect("RedmineのAPIキーが指定されていません");
         let discord_executor_role_id = env::var("EXECUTABLE_ROLE_ID")
@@ -29,6 +32,7 @@ impl Env {
 
         Self {
             discord_token,
+            discord_application_id,
             redmine_api_key,
             discord_executor_role_id,
             github_app_id,
