c09291144a7c17aa2ad0582fd1c06f840685d467
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-19 22:31:21 +0900

    fix: use AgendaId for Agenda

--- src/domains/agenda.rs
+++ src/domains/agenda.rs
@@ -1,9 +1,19 @@
-use super::status::AgendaStatus;
+use super::{id::AgendaId, status::AgendaStatus};
 use serenity::model::id::MessageId;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub struct Agenda {
-    pub id: u16,
+    pub id: AgendaId,
     pub status: AgendaStatus,
     pub votes_message_id: Option<MessageId>,
+}
+
+impl Agenda {
+    pub fn new(id: u16) -> Self {
+        Self {
+            id: AgendaId::new(id),
+            status: AgendaStatus::New,
+            votes_message_id: None,
+        }
+    }
 }
