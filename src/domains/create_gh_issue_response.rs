cebfd7a1eb5c51a375aa339f6916bf43d313e816
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-19 22:41:42 +0900

    docs: add docs for CreateGitHubIssueResponse

--- src/domains/create_gh_issue_response.rs
+++ src/domains/create_gh_issue_response.rs
@@ -1,6 +1,8 @@
 use serde::Deserialize;
 
+/// GitHubにIssueを作成したときのResponseをデシリアライズするための構造体
 #[derive(Deserialize)]
 pub struct CreateIssueResponse {
+    /// 作成したIssueのURL
     pub html_url: String,
 }
