30cb6e7d0cdfa1e8234751c11579e3df03faabbb
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-19 23:41:24 +0900

    feat: add CreateEmbedExt

--- src/util/discord_embed.rs
+++ src/util/discord_embed.rs
@@ -1,47 +1,49 @@
-use crate::domain::{redmine, status::AgendaStatus};
+use crate::domain::{id::IssueId, redmine, status::AgendaStatus};
 use chrono::Utc;
 use regex::Regex;
 use serenity::{builder, utils::Color};
 
-pub fn default_embed(
-    embed: &mut builder::CreateEmbed,
-    record_id: u16,
-) -> &mut builder::CreateEmbed {
-    embed
-        .timestamp(Utc::now().to_rfc3339())
-        .footer(|footer| footer.text(format!("アイデア会議: #{}", record_id)))
+pub trait CreateEmbedExt {
+    fn timestamp(&mut self) -> &mut Self;
+    fn with_record_id(&mut self, record_id: IssueId) -> &mut Self;
+    fn simple_color(&mut self) -> &mut Self;
+    fn success_color(&mut self) -> &mut Self;
+    fn failure_color(&mut self) -> &mut Self;
 }
 
-pub fn default_colored_embed(
-    embed: &mut builder::CreateEmbed,
-    record_id: u16,
-) -> &mut builder::CreateEmbed {
-    default_embed(embed, record_id).color(Color::from_rgb(179, 159, 159))
-}
+impl CreateEmbedExt for builder::CreateEmbed {
+    fn timestamp(&mut self) -> &mut Self {
+        self.timestamp(Utc::now().to_rfc3339())
+    }
 
-pub fn default_success_embed(
-    embed: &mut builder::CreateEmbed,
-    record_id: u16,
-) -> &mut builder::CreateEmbed {
-    default_embed(embed, record_id).color(Color::from_rgb(50, 173, 240))
-}
+    fn with_record_id(&mut self, record_id: IssueId) -> &mut Self {
+        self.footer(|footer| footer.text(format!("アイデア会議: #{}", record_id.0)))
+    }
 
-pub fn default_failure_embed(
-    embed: &mut builder::CreateEmbed,
-    record_id: u16,
-) -> &mut builder::CreateEmbed {
-    default_embed(embed, record_id).color(Color::from_rgb(245, 93, 93))
+    fn simple_color(&mut self) -> &mut Self {
+        self.color(Color::from_rgb(179, 159, 159))
+    }
+
+    fn success_color(&mut self) -> &mut Self {
+        self.color(Color::from_rgb(50, 173, 240))
+    }
+
+    fn failure_color(&mut self) -> &mut Self {
+        self.color(Color::from_rgb(245, 93, 93))
+    }
 }
 
 pub fn next_agenda_embed(
     embed: &mut builder::CreateEmbed,
-    record_id: u16,
+    record_id: IssueId,
     next_redmine_issue: Option<redmine::RedmineIssue>,
 ) -> &mut builder::CreateEmbed {
     if let Some(issue) = next_redmine_issue {
         let reg = Regex::new(r"^\[.*\]\s").unwrap();
         let subject = reg.replace(&issue.subject, "");
-        default_colored_embed(embed, record_id)
+        embed
+            .simple_color()
+            .with_record_id(record_id)
             .title(format!("次の議題は#{}です", issue.id))
             .field(
                 "議題チケット",
@@ -51,7 +53,9 @@ pub fn next_agenda_embed(
             .field("タイトル", subject, false)
             .field("説明", issue.description, false)
     } else {
-        default_failure_embed(embed, record_id)
+        embed
+            .failure_color()
+            .with_record_id(record_id)
             .title("次の議題はありません")
             .description("Redmine上で提起されていた議題は全て処理されました。")
     }
@@ -59,25 +63,28 @@ pub fn next_agenda_embed(
 
 pub fn no_current_agenda_embed(
     embed: &mut builder::CreateEmbed,
-    record_id: u16,
+    record_id: IssueId,
 ) -> &mut builder::CreateEmbed {
-    default_failure_embed(embed, record_id).title("現在進行中の議題はありません")
+    embed
+        .failure_color()
+        .with_record_id(record_id)
+        .title("現在進行中の議題はありません")
 }
 
 pub fn votes_result_embed(
     embed: &mut builder::CreateEmbed,
-    record_id: u16,
-    current_agenda_id: u16,
+    record_id: IssueId,
+    current_agenda_id: IssueId,
     status: AgendaStatus,
 ) -> &mut builder::CreateEmbed {
     match status {
-        AgendaStatus::Approved => default_success_embed(embed, record_id),
-        AgendaStatus::Declined => default_failure_embed(embed, record_id),
+        AgendaStatus::Approved => embed.success_color().with_record_id(record_id),
+        AgendaStatus::Declined => embed.failure_color().with_record_id(record_id),
         _ => embed,
     }
     .title(format!(
         "採決終了: #{}は{}されました",
-        current_agenda_id,
+        current_agenda_id.0,
         status.ja()
     ))
 }
