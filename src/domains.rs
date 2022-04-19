2b858bc2610af531c1b6b3b54c92843f3837238c
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-19 22:38:34 +0900

    refactor: rename github -> create_gh_issue_response

--- src/domains.rs
+++ src/domains.rs
@@ -1,8 +1,10 @@
 pub mod custom_error;
-pub mod github;
 pub mod id;
 pub mod redmine;
 pub mod status;
 
 mod agenda;
 pub use agenda::Agenda;
+
+mod create_gh_issue_response;
+pub use create_gh_issue_response::CreateIssueResponse;
