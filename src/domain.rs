f8069df40ffbc25613c3f643dea0fdecbab9b3e0
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-20 15:12:59 +0900

    feat: add custom Error

--- src/domain.rs
+++ src/domain.rs
@@ -3,6 +3,9 @@ pub mod id;
 pub mod redmine;
 pub mod status;
 
+mod error;
+pub use error::MyError;
+
 mod agenda;
 pub use agenda::Agenda;
 
---
+++ src/domain/error.rs
@@ -0,0 +1,23 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum MyError {
+    #[error("引数が指定されていません。")]
+    ArgIsNotSpecified,
+    #[error("チケットが存在しません。")]
+    TicketIsNotFound,
+    #[error("チケットはアイデア提案用プロジェクトのアイデアではありません。")]
+    TicketIsNotIdea,
+    #[error("チケットはアイデア提案用プロジェクトの未議論アイデアではありません。")]
+    TicketIsNotUndoneIdea,
+    #[error("チケットはアイデア会議議事録プロジェクトの議事録ではありません。")]
+    TicketIsNotIdeaDiscussionRecord,
+    #[error("チケットはアイデア会議議事録プロジェクトの未終了議事録ではありません。")]
+    TicketIsNotUndoneIdeaDiscussionRecord,
+    #[error("チケットのステータスは終了ステータスとして適切なものではありません。")]
+    TicketStatusIsNotDone,
+    #[error("VCに参加されていないようです。")]
+    IsNotJoinedInVC,
+    #[error(transparent)]
+    Http(#[from] reqwest::Error),
+}
