b0fe0eedfa86488c179200f3e6a49f5d41bf8dfc
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-26 15:12:07 +0900

    refactor: change arg name

--- src/domain/repository/record.rs
+++ src/domain/repository/record.rs
@@ -7,6 +7,6 @@ use serenity::async_trait;
 #[async_trait]
 pub trait RecordRepository {
     async fn list(&self) -> Vec<Record>;
-    async fn update(&self, record: Record);
+    async fn update(&self, new_record: Record);
     async fn add_note(&self, id: IssueId, note: Note);
 }
