26742e70bdcba303fc26b4ac7f77f667a039d88f
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-26 12:27:15 +0900

    refactor: rename create_gh_issue -> gh_issue

--- src/domain.rs
+++ src/domain.rs
@@ -9,5 +9,4 @@ pub use error::MyError;
 mod agenda;
 pub use agenda::Agenda;
 
-mod create_gh_issue_response;
-pub use create_gh_issue_response::CreateIssueResponse;
+pub mod gh_issue;
--- src/domain/gh_issue.rs
+++ src/domain/gh_issue.rs
@@ -1,5 +1,6 @@
 use serde::Deserialize;
 
+// TODO: ここではない
 /// GitHubにIssueを作成したときのResponseをデシリアライズするための構造体
 #[derive(Deserialize)]
 pub struct CreateIssueResponse {
