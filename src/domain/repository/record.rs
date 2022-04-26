b0997398c61e353e449c9d8b3cb9ec39d9d16d12
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-26 15:07:15 +0900

    feat: add add_note method to RecordRepo

--- src/domain/repository/record.rs
+++ src/domain/repository/record.rs
@@ -1,8 +1,12 @@
-use crate::domain::ticket::Record;
+use crate::domain::{
+    id::IssueId,
+    ticket::{Note, Record},
+};
 use serenity::async_trait;
 
 #[async_trait]
 pub trait RecordRepository {
     async fn list(&self) -> Vec<Record>;
     async fn update(&self, record: Record);
+    async fn add_note(&self, id: IssueId, note: Note);
 }
