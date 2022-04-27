6ccdce27a9cc003ca602ec992e820fd86275a247
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-27 13:12:36 +0900

    feat: add infra dir

---
+++ src/infrastructure.rs
@@ -0,0 +1,3 @@
+mod model;
+pub mod persistence;
+pub mod repository;
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod domain;
 pub mod global;
+pub mod infrastructure;
 pub mod presentation;
 pub mod usecase;
 pub mod util;
