a87aba8f38392add2bf092325a802e1e26e94c64
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-27 15:31:27 +0900

    feat(StatusExt): add from_id method

--- src/domain/status.rs
+++ src/domain/status.rs
@@ -8,13 +8,22 @@ pub use record_status::RecordStatus;
 
 pub mod record;
 
+use strum::{EnumProperty, IntoEnumIterator};
+
 pub trait StatusExt {
     fn id(&self) -> u16
     where
-        Self: strum::EnumProperty,
+        Self: EnumProperty,
     {
         self.get_str("id")
             .and_then(|str| str.parse::<u16>().ok())
             .unwrap_or(1)
+    }
+
+    fn from_id(id: u16) -> Option<Self>
+    where
+        Self: IntoEnumIterator + EnumProperty,
+    {
+        Self::iter().find(|s| s.id() == id)
     }
 }
