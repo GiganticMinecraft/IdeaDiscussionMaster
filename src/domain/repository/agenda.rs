f920464216d11d5e139178e3df0007df2dad661e
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-27 13:10:09 +0900

    fix: change return type of Repository#find to Result

--- src/domain/repository/agenda.rs
+++ src/domain/repository/agenda.rs
@@ -6,7 +6,7 @@ use serenity::async_trait;
 
 #[async_trait]
 pub trait AgendaRepository {
-    async fn find(&self, id: IssueId) -> Option<Agenda>;
+    async fn find(&self, id: IssueId) -> anyhow::Result<Agenda>;
     async fn update(&self, new_agenda: Agenda);
     async fn add_note(&self, id: IssueId, note: Note);
 }
