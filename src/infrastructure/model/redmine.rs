bc7e531413dd662783afb2eff5af1db943969950
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-27 13:11:20 +0900

    feat: add redmine serealizer

---
+++ src/infrastructure/model/redmine.rs
@@ -0,0 +1,82 @@
+use crate::domain::{id::IssueId, status::agenda::AgendaStatus, ticket::Agenda};
+use itertools::Itertools;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Default, PartialEq)]
+pub struct RedmineProject {
+    pub name: String,
+}
+
+#[derive(Debug, Deserialize, Default, PartialEq)]
+pub struct RedmineIssueTracker {
+    pub name: String,
+}
+
+#[derive(Debug, Deserialize, Default, PartialEq)]
+pub struct RedmineIssueStatus {
+    pub name: String,
+}
+
+#[derive(Debug, Deserialize, Default, PartialEq)]
+pub struct RedmineIssueRelations {
+    pub issue_id: u16,
+    pub issue_to_id: u16,
+    pub relation_type: String,
+}
+
+#[derive(Debug, Deserialize, Default, PartialEq)]
+pub struct RedmineIssue {
+    pub id: u16,
+    pub project: RedmineProject,
+    pub tracker: RedmineIssueTracker,
+    pub status: RedmineIssueStatus,
+    pub subject: String,
+    pub description: String,
+    #[serde(default)]
+    pub relations: Vec<RedmineIssueRelations>,
+}
+
+impl RedmineIssue {
+    pub fn is_idea_ticket(&self) -> bool {
+        self.project.name == "アイデア提案用プロジェクト" && self.tracker.name == "アイデア提案"
+    }
+
+    pub fn is_undone_idea_ticket(&self) -> bool {
+        self.is_idea_ticket()
+            && !AgendaStatus::closed()
+                .iter()
+                .map(|status| status.ja())
+                .contains(&self.status.name)
+    }
+
+    pub fn is_idea_discussion_record(&self) -> bool {
+        self.project.name == "アイデア会議議事録" && self.tracker.name == "アイデア会議"
+    }
+
+    pub fn is_undone_idea_discussion_record(&self) -> bool {
+        self.is_idea_discussion_record() && self.status.name == AgendaStatus::New.ja()
+    }
+
+    pub fn relations(&self) -> Vec<u16> {
+        self.relations
+            .iter()
+            .filter(|rel| rel.relation_type == "relates")
+            .flat_map(|rel| vec![rel.issue_id, rel.issue_to_id])
+            .collect_vec()
+    }
+}
+
+#[derive(Debug, Deserialize, Default, PartialEq)]
+pub struct RedmineIssueResult {
+    pub issue: RedmineIssue,
+}
+
+impl Into<Agenda> for RedmineIssueResult {
+    fn into(self) -> Agenda {
+        Agenda::new(
+            IssueId::new(self.issue.id),
+            self.issue.subject,
+            self.issue.description,
+        )
+    }
+}
