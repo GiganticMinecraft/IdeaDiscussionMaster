b398331493b4478f30c32de802a8db66926ee934
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-28 15:18:34 +0900

    fix: change return type of record repo

--- src/domain/repository/record.rs
+++ src/domain/repository/record.rs
@@ -6,8 +6,8 @@ use serenity::async_trait;
 
 #[async_trait]
 pub trait RecordRepository {
-    async fn list(&self) -> Vec<Record>;
-    async fn update(&self, new_record: Record);
-    async fn add_note(&self, id: IssueId, note: Note);
-    async fn add_relation(&self, id: IssueId, relate_id: IssueId);
+    async fn list(&self) -> anyhow::Result<Vec<Record>>;
+    async fn update(&self, new_record: Record) -> anyhow::Result<()>;
+    async fn add_note(&self, id: IssueId, note: Note) -> anyhow::Result<()>;
+    async fn add_relation(&self, id: IssueId, relate_id: IssueId) -> anyhow::Result<()>;
 }
--- src/usecase/record.rs
+++ src/usecase/record.rs
@@ -2,8 +2,9 @@ use super::model::RecordDto;
 use crate::domain::{
     id::IssueId,
     repository::RecordRepository,
-    status::record::RecordStatus,
+    status::{record::RecordStatus, StatusExt},
     ticket::{Note, Record},
+    MyError,
 };
 use derive_new::new;
 use std::sync::Arc;
@@ -14,48 +15,46 @@ pub struct RecordUseCase<R: RecordRepository> {
 }
 
 impl<R: RecordRepository> RecordUseCase<R> {
-    pub async fn find_new_one(&self) -> Option<RecordDto> {
-        self.find(|ticket| ticket.status == RecordStatus::New)
+    pub async fn find_new_one(&self) -> anyhow::Result<RecordDto> {
+        self.find(|ticket| ticket.status.is_new())
             .await
             .map(|r| r.into())
     }
 
-    async fn find_by_id_with_record(&self, id: IssueId) -> Option<Record> {
+    async fn find_by_id_with_record(&self, id: IssueId) -> anyhow::Result<Record> {
         self.find(|ticket| ticket.id == id).await
     }
 
-    pub async fn find_by_id(&self, id: IssueId) -> Option<RecordDto> {
+    pub async fn find_by_id(&self, id: IssueId) -> anyhow::Result<RecordDto> {
         self.find_by_id_with_record(id).await.map(|r| r.into())
     }
 
-    pub async fn add_note(&self, id: IssueId, note: Note) {
+    pub async fn add_note(&self, id: IssueId, note: Note) -> anyhow::Result<()> {
         self.repository.add_note(id, note).await
     }
 
-    pub async fn close(&self, id: IssueId) {
-        let record = self.find_by_id_with_record(id).await;
+    pub async fn close(&self, id: IssueId) -> anyhow::Result<()> {
+        let record = self.find_by_id_with_record(id).await?;
+        let new = record.close();
 
-        if let Some(record) = record {
-            let new = record.close();
-            self.repository.update(new).await;
-        }
+        self.repository.update(new).await
     }
 
-    pub async fn add_relation(&self, id: IssueId, relation: IssueId) {
-        self.repository.add_relation(id, relation).await;
+    pub async fn add_relation(&self, id: IssueId, relation: IssueId) -> anyhow::Result<()> {
+        self.repository.add_relation(id, relation).await
     }
 
     // TODO: 切り出す
     #[allow(dead_code)]
-    async fn find<P>(&self, f: P) -> Option<Record>
+    async fn find<P>(&self, f: P) -> anyhow::Result<Record>
     where
         P: FnMut(&&Record) -> bool,
     {
-        self.repository
-            .list()
-            .await
-            .iter()
+        let list = self.repository.list().await?;
+
+        list.iter()
             .find(f)
             .map(|ticket| ticket.to_owned())
+            .ok_or_else(|| MyError::TicketIsNotFound.into())
     }
 }
