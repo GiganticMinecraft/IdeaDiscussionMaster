583e78402ce94a54f9ffdae5d7541c21b3a027e6
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-04-29 23:16:10 +0900

    feat: add Module global var

--- src/global.rs
+++ src/global.rs
@@ -1,3 +1,4 @@
 pub mod agendas;
+pub mod module;
 pub mod record_id;
 pub mod voice_chat_channel_id;
---
+++ src/global/module.rs
@@ -0,0 +1,15 @@
+use crate::presentation::module::Module;
+use once_cell::sync::Lazy;
+use std::sync::{Arc, Mutex};
+
+static MODULE: Lazy<Arc<Mutex<Option<Module>>>> = Lazy::new(|| Arc::new(Mutex::new(None)));
+
+pub async fn init() {
+    let module = Module::new().await;
+
+    *MODULE.lock().unwrap() = Some(module);
+}
+
+pub fn get() -> Option<Module> {
+    MODULE.lock().unwrap().clone()
+}
