1ae1ba5c6c6f1d2f7eb92ad042c523813a66c662
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-05-04 12:09:29 +0900

    refactor: fix error print style

--- crate-presentation/src/handler.rs
+++ crate-presentation/src/handler.rs
@@ -2,12 +2,12 @@ use super::command;
 use crate_utils::{
     command::{
         application_interaction::{ApplicationInteractions, SlashCommand},
-        CommandExt, CommandInteraction, InteractionResponse, Parser,
+        ArgsMap, CommandExt, CommandInteraction, InteractionResponse, Parser,
     },
     SerenityContext,
 };
 
-use anyhow::Context;
+use anyhow::{anyhow, Context};
 use serenity::{
     async_trait,
     builder::CreateEmbed,
@@ -30,7 +30,7 @@ impl EventHandler for Handler {
             .await
             .context("Error while creating slash commands")
         {
-            println!("{:#?}", e);
+            println!("{:?}", e);
         };
 
         let interactions = ApplicationCommand::get_global_application_commands(&ctx.http).await;
@@ -58,7 +58,7 @@ impl EventHandler for Handler {
         if let Some(command) = interaction.clone().application_command() {
             let response = create_interaction(&command, &ctx)
                 .await
-                .unwrap_or_else(|m| InteractionResponse::Message(m.to_string()));
+                .unwrap_or_else(|m| InteractionResponse::Message(format!("{:?}", m)));
 
             let _ = match response {
                 InteractionResponse::Message(m) => command.message(&ctx.http, m).await,
@@ -83,23 +83,34 @@ async fn create_interaction(
     interaction: &CommandInteraction,
     context: &SerenityContext,
 ) -> anyhow::Result<InteractionResponse> {
-    let data = interaction.data.parse()?;
-    let (cmd, args) = data.split_first().unwrap();
-    let cmd = match &cmd.1 {
-        ApplicationInteractions::SlashCommand(SlashCommand::Command(cmd))
-            if command::all_command_names().contains(cmd) =>
-        {
-            Ok(cmd)
-        }
-        _ => Err(anyhow::anyhow!("Unexpected interaction")),
-    }?;
-    let args = args.iter().cloned().collect::<HashMap<_, _>>();
+    let (cmd, args) = split_of(interaction)?;
+    let sub_command = args.get("sub_command").and_then(|i| match i {
+        ApplicationInteractions::SlashCommand(SlashCommand::SubCommand(name)) => Some(name.clone()),
+        _ => None,
+    });
+    let fn_args = (args, context.to_owned(), interaction.to_owned());
 
-    let response = command::executor(cmd)(args, context.to_owned(), interaction.to_owned())
-        .await
-        .context("Error while creating a response")?;
+    let error = anyhow!("予期していないコマンドです。");
+    let result = match cmd.as_str() {
+        "start" => command::start::executor(fn_args).await,
+        "end" => command::end::executor(fn_args).await,
+        "vote" => command::vote::executor(fn_args).await,
+        "agenda" => match sub_command.unwrap().as_str() {
+            "add" => command::agenda::add(fn_args).await,
+            "list" => command::agenda::list(fn_args).await,
+            "set" => command::agenda::set(fn_args).await,
+            _ => Err(error),
+        },
+        "create" => match sub_command.unwrap().as_str() {
+            "new_record" => command::create::new_record(fn_args).await,
+            "issue" => command::create::issue(fn_args).await,
+            _ => Err(error),
+        },
+        _ => Err(error),
+    }
+    .context("コマンドの処理中にエラーが発生しました。")?;
 
-    Ok(match response {
+    Ok(match result {
         InteractionResponse::Message(m) if m == *"" => {
             InteractionResponse::Message("Success: There is no message".to_string())
         }
@@ -112,4 +123,20 @@ async fn create_interaction(
         }
         res => res,
     })
+}
+
+fn split_of(interaction: &CommandInteraction) -> anyhow::Result<(String, ArgsMap)> {
+    let data = interaction.data.parse()?;
+    let (cmd, args) = data.split_first().unwrap();
+    let cmd = match &cmd.1 {
+        ApplicationInteractions::SlashCommand(SlashCommand::Command(cmd))
+            if command::all_command_names().contains(cmd) =>
+        {
+            Ok(cmd)
+        }
+        _ => Err(anyhow::anyhow!("Unexpected interaction")),
+    }?;
+    let args = args.iter().cloned().collect::<HashMap<_, _>>();
+
+    Ok((cmd.to_owned(), args))
 }
