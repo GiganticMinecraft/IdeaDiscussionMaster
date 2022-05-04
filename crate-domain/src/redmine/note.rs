80bf28db5e7f3d51881740b41e5cf4857fcb6065
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-05-04 23:48:16 +0900

    fix(domain): remove author field from Note

--- crate-domain/src/redmine/note.rs
+++ crate-domain/src/redmine/note.rs
@@ -2,12 +2,11 @@ use derive_new::new;
 
 #[derive(Debug, Clone, new)]
 pub struct Note {
-    pub author: String,
     pub content: Vec<String>,
 }
 
 impl Note {
-    pub fn from_string_content(author: String, content: String) -> Self {
-        Self::new(author, content.split('\n').map(|s| s.to_owned()).collect())
+    pub fn from_string_content(content: String) -> Self {
+        Self::new(content.split('\n').map(|s| s.to_owned()).collect())
     }
 }
