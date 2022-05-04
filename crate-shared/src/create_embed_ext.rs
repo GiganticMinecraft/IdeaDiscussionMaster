8437aead1b0a235d8c5960247ab4a0cac29f7332
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-05-04 22:57:52 +0900

    feat(shared): add record_url_field to CreateEmbedExt

--- crate-shared/src/create_embed_ext.rs
+++ crate-shared/src/create_embed_ext.rs
@@ -1,3 +1,4 @@
+use super::REDMINE_URL;
 use crate_domain::id::IssueId;
 
 use chrono::Utc;
@@ -10,6 +11,7 @@ pub trait CreateEmbedExt {
     fn simple_color(&mut self) -> &mut Self;
     fn success_color(&mut self) -> &mut Self;
     fn failure_color(&mut self) -> &mut Self;
+    fn record_url_field(&mut self, record_id: &IssueId) -> &mut Self;
 }
 
 impl CreateEmbedExt for builder::CreateEmbed {
@@ -35,5 +37,13 @@ impl CreateEmbedExt for builder::CreateEmbed {
 
     fn failure_color(&mut self) -> &mut Self {
         self.color(Color::from_rgb(245, 93, 93))
+    }
+
+    fn record_url_field(&mut self, record_id: &IssueId) -> &mut Self {
+        self.field(
+            "議事録チケット",
+            format!("{}/issues/{}", REDMINE_URL, record_id.0),
+            false,
+        )
     }
 }
