5758ee3e1d440d412e9ed0a8d3bde0371f05d9fa
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-05-04 21:51:07 +0900

    feat(presentation): implement start command

--- crate-presentation/src/command/start.rs
+++ crate-presentation/src/command/start.rs
@@ -1,14 +1,19 @@
-use super::super::{global, module::ModuleExt};
+use super::super::{global, module::ModuleExt, utils::discord_embeds};
 use crate_domain::id::IssueId;
-use crate_utils::{
+use crate_shared::{
     self,
     command::{
         builder::{SlashCommandBuilder, SlashCommandOptionBuilder},
         CommandResult, ExecutorArgs, InteractionResponse,
     },
+    CreateEmbedExt, REDMINE_URL,
 };
 
-use serenity::model::interactions::application_command::ApplicationCommandOptionType;
+use futures::stream::{self, StreamExt};
+use itertools::Itertools;
+use serenity::{
+    builder::CreateEmbed, model::interactions::application_command::ApplicationCommandOptionType,
+};
 
 pub fn builder() -> SlashCommandBuilder {
     SlashCommandBuilder::new("start", "アイデア会議を開始します。")
@@ -26,25 +31,92 @@ pub fn builder() -> SlashCommandBuilder {
 }
 
 pub async fn executor((map, ctx, interaction): ExecutorArgs) -> CommandResult {
-    let vc_id = crate_utils::find_vc_by_user_id(
-        &ctx.cache,
-        &interaction.guild_id.unwrap(),
-        &interaction.user.id,
-    )
-    .await?;
-    global::voice_chat_channel_id::update(vc_id);
+    let module = global::module::get();
 
+    // VCへの参加状況を取得
+    // 参加していればグローバル変数にそのVCのChannelIdを格納
+    // 参加していなければ終了
+    // let vc_id = crate_shared::find_vc_by_user_id(
+    //     &ctx.cache,
+    //     &interaction.guild_id.unwrap(),
+    //     &interaction.user.id,
+    // )
+    // .await?;
+    // global::voice_chat_channel_id::update(vc_id);
+
+    // 議事録を取得
+    // 存在しなければ終了
     let record_id: u16 = map
         .get("discussion_issue_number")
         .unwrap()
         .to_owned()
         .try_into()?;
     let record_id = IssueId::new(record_id);
-    let module = global::module::get();
     let record = module.record_usecase().find_new(record_id).await?;
 
-    Ok(InteractionResponse::Message(format!(
-        "会議を開始しました。\n {:?}",
-        record
-    )))
+    // 議題を取得
+    let agendas: Vec<_> = stream::iter(&record.relations)
+        .then(|id| module.agenda_usecase().find_new(*id))
+        .filter_map(|res| async { res.ok() })
+        .collect()
+        .await;
+    // グローバル変数の議題をリセットして、取得した議題を追加
+    global::agendas::clear();
+    agendas
+        .clone()
+        .into_iter()
+        .map(|dto| dto.into())
+        .for_each(|agenda| {
+            global::agendas::add(agenda);
+        });
+
+    println!("Discussion started: #{}", record.id.0);
+    println!(
+        "Agendas({}): {:?}",
+        agendas.len(),
+        agendas
+            .iter()
+            .map(|agenda| format!("{:?}", agenda.id))
+            .join(", ")
+    );
+
+    let beginning_embed = CreateEmbed::default()
+        .custom_default(&record_id)
+        .title("会議を開始しました")
+        .field(
+            "議事録チケット",
+            format!("{}/issues/{}", REDMINE_URL, record_id.0),
+            false,
+        )
+        .to_owned();
+
+    // 次の議題を選択
+    let next_agenda_id = global::agendas::find_next();
+    // 次の議題があれば、グローバル変数の中の議題のステータスを進行中に変更
+    if let Some(id) = next_agenda_id {
+        global::agendas::in_progress(id);
+    }
+    // 次の議題の存否に応じてEmbedを作成
+    let mut agenda_embed = CreateEmbed::default();
+    let agenda_embed = match next_agenda_id {
+        Some(id) => {
+            let next_agenda = module.agenda_usecase().find_new(id).await?;
+
+            discord_embeds::next_agenda_embed(&mut agenda_embed, &record.id, &next_agenda)
+        }
+        None => discord_embeds::no_next_agenda(&mut agenda_embed, &record.id),
+    }
+    .to_owned();
+
+    println!(
+        "Next Agenda: {}",
+        next_agenda_id
+            .map(|id| id.0.to_string())
+            .unwrap_or_else(|| "-".to_string())
+    );
+
+    Ok(InteractionResponse::Embeds(vec![
+        beginning_embed,
+        agenda_embed,
+    ]))
 }
