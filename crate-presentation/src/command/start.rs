e52577b9d432461559c6ac1460744aa7bd9c8bf3
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-05-05 23:57:20 +0900

    refactor(presentation): use field for url

--- crate-presentation/src/command/start.rs
+++ crate-presentation/src/command/start.rs
@@ -75,7 +75,11 @@ pub async fn executor((map, ctx, interaction): ExecutorArgs) -> CommandResult {
     let beginning_embed = CreateEmbed::default()
         .custom_default(&record_id)
         .title("会議を開始しました")
-        .description(format!("{}/issues/{}", REDMINE_URL, record_id.0))
+        .field(
+            "議事録チケット",
+            format!("{}/issues/{}", REDMINE_URL, record_id.0),
+            false,
+        )
         .to_owned();
 
     println!("Discussion started: #{}", record_id.0);
