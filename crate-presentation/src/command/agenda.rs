27596a3fa22e51e1d3f0b99756cac26522264a65
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-05-06 00:04:12 +0900

    feat(presentation): add agenda add command

--- crate-presentation/src/command/agenda.rs
+++ crate-presentation/src/command/agenda.rs
@@ -1,7 +1,11 @@
-use super::super::{global, utils::discord_embeds};
-use crate_shared::command::{
-    builder::{SlashCommandBuilder, SlashCommandOptionBuilder},
-    CommandResult, ExecutorArgs, InteractionResponse,
+use super::super::{global, module::ModuleExt, utils::discord_embeds};
+use crate_domain::id::IssueId;
+use crate_shared::{
+    command::{
+        builder::{SlashCommandBuilder, SlashCommandOptionBuilder},
+        CommandResult, ExecutorArgs, InteractionResponse,
+    },
+    CreateEmbedExt,
 };
 
 use serenity::{
@@ -34,8 +38,55 @@ pub fn builder() -> SlashCommandBuilder {
         .into()
 }
 
-pub async fn add((_map, _ctx, _interaction): ExecutorArgs) -> CommandResult {
-    Ok(InteractionResponse::Message("add".to_string()))
+pub async fn add((map, _ctx, _interaction): ExecutorArgs) -> CommandResult {
+    let module = global::module::get();
+
+    // 新規議題を取得し、グローバル変数に格納
+    // 新規議題が存在しなければ、終了
+    let new_agenda_id: u16 = map
+        .get("idea_issue_number")
+        .unwrap()
+        .to_owned()
+        .try_into()?;
+    let new_agenda_id = IssueId::new(new_agenda_id);
+    let new_agenda = module.agenda_usecase().find_new(new_agenda_id).await?;
+    global::agendas::add(new_agenda.clone().into());
+
+    // 議事録チケットと関連付ける
+    let record_id = global::record_id::get().unwrap();
+    println!("{} {}", record_id.0, new_agenda_id.0);
+    module
+        .record_usecase()
+        .add_relation(record_id, new_agenda_id)
+        .await?;
+
+    let add_agenda_embed = CreateEmbed::default()
+        .custom_default(&record_id)
+        .title("議題を追加しました")
+        .description(format!("追加した議題: #{}", new_agenda_id.0))
+        .success_color()
+        .to_owned();
+
+    println!("Agenda added: #{}", new_agenda_id.0);
+
+    // 現在進行中の議題があれば何もせず、なければ議題として提示
+    Ok(match global::agendas::find_current() {
+        Some(_) => InteractionResponse::Embed(add_agenda_embed),
+        None => {
+            assert!(global::agendas::find_next().is_some());
+
+            global::agendas::in_progress(new_agenda_id);
+
+            let mut agenda_embed = CreateEmbed::default();
+            let agenda_embed =
+                discord_embeds::next_agenda_embed(&mut agenda_embed, &record_id, &new_agenda)
+                    .to_owned();
+
+            println!("Next Agenda: #{}", new_agenda_id.0);
+
+            InteractionResponse::Embeds(vec![add_agenda_embed, agenda_embed])
+        }
+    })
 }
 
 pub async fn list((_map, _ctx, _interaction): ExecutorArgs) -> CommandResult {
