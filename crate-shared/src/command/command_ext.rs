38d0f3d4ca7edb192362f5bdff2c007f8bbbd414
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-05-12 15:24:31 +0900

    refactor(presentation): add interaction parser for Ext

--- crate-shared/src/command/command_ext.rs
+++ crate-shared/src/command/command_ext.rs
@@ -1,8 +1,12 @@
-use super::CommandInteraction;
+use super::{
+    application_interaction::{ApplicationInteractions, SlashCommand},
+    ArgsMap, CommandInteraction, Parser,
+};
 
 use anyhow::Context;
 use itertools::Itertools;
 use serenity::{async_trait, builder::CreateEmbed, http::Http, model::id::MessageId};
+use std::collections::HashMap;
 
 #[async_trait]
 pub trait CommandExt {
@@ -26,6 +30,8 @@ pub trait CommandExt {
         http: impl AsRef<Http> + Send + Sync + 'async_trait,
         embeds: Vec<CreateEmbed>,
     ) -> anyhow::Result<MessageId>;
+
+    async fn split_of(&self) -> anyhow::Result<(String, ArgsMap)>;
 }
 
 #[async_trait]
@@ -68,5 +74,17 @@ impl CommandExt for CommandInteraction {
             .await
             .map(|msg| msg.id)
             .context("Cannot to edit interaction response!")
+    }
+
+    async fn split_of(&self) -> anyhow::Result<(String, ArgsMap)> {
+        let data = self.data.parse()?;
+        let (cmd, args) = data.split_first().unwrap();
+        let cmd = match &cmd.1 {
+            ApplicationInteractions::SlashCommand(SlashCommand::Command(cmd)) => Ok(cmd),
+            _ => Err(anyhow::anyhow!("Unexpected interaction")),
+        }?;
+        let args = args.iter().cloned().collect::<HashMap<_, _>>();
+
+        Ok((cmd.to_owned(), args))
     }
 }
