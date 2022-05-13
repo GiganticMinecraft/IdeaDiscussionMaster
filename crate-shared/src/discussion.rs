fe4fb589d5435e914e9e9af07f3903fb8b14dea0
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-05-13 15:48:24 +0900

    refactor: remove comments

--- crate-shared/src/discussion.rs
+++ crate-shared/src/discussion.rs
@@ -23,21 +23,3 @@ pub async fn find_vc_by_user_id(
         .and_then(|state| state.channel_id)
         .ok_or_else(|| MyError::IsNotJoinedInVC.into())
 }
-
-// TODO: return Result
-
-// TODO: なおす
-
-// pub async fn go_to_next_agenda(ctx: &Context) -> Option<u16> {
-//     let cached_agendas = agendas::read(ctx).await;
-//     let agenda_id = cached_agendas
-//         .iter()
-//         .find(|(_, agenda)| agenda.status.is_new())
-//         .map(|(id, _)| id.to_owned());
-
-//     if agenda_id.is_some() {
-//         agendas::update_status(ctx, agenda_id.unwrap(), AgendaStatus::InProgress).await;
-//     }
-
-//     agenda_id
-// }
