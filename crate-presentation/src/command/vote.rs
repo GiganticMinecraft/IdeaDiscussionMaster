60a664695c68e07d08cbe63bcf111d21d8315b4f
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-05-13 15:10:29 +0900

    feat(presentation): impl vote end command

--- crate-presentation/src/command/vote.rs
+++ crate-presentation/src/command/vote.rs
@@ -1,4 +1,5 @@
-use super::super::global;
+use super::super::{global, module::ModuleExt, utils::discord_embeds};
+use crate_domain::status::AgendaStatus;
 use crate_shared::{
     command::{
         builder::{SlashCommandBuilder, SlashCommandOptionBuilder},
@@ -11,6 +12,7 @@ use anyhow::{bail, ensure};
 use serenity::{
     builder::CreateEmbed, model::interactions::application_command::ApplicationCommandOptionType,
 };
+use std::str::FromStr;
 
 pub fn builder() -> SlashCommandBuilder {
     SlashCommandBuilder::new("vote", "投票を行います。")
@@ -71,6 +73,78 @@ pub async fn start((_map, _ctx, _interaction): ExecutorArgs) -> CommandResult {
     Ok(InteractionResponse::Embed(embed))
 }
 
-pub async fn end((_map, _ctx, _interaction): ExecutorArgs) -> CommandResult {
-    Ok(InteractionResponse::Message("vote end".to_string()))
+pub async fn end((map, _ctx, _interaction): ExecutorArgs) -> CommandResult {
+    let module = global::module::get();
+
+    // ステータスなど各種必要な変数を取得
+    let status: String = map.get("status").unwrap().to_owned().try_into()?;
+    let status = AgendaStatus::from_str(&status).unwrap();
+    let record_id = global::record_id::get().unwrap();
+    let current_agenda = match global::agendas::find_current() {
+        Some(agenda) => agenda,
+        None => {
+            bail!("現在進行中の議題はありません。")
+        }
+    };
+
+    // 投票メッセージの記録をリセット
+    if let Some(id) = current_agenda.votes_message_id {
+        global::agendas::update_votes_message_id(current_agenda.id, None);
+    };
+
+    // 投票結果のEmbedを作成
+    let mut vote_result_embed = CreateEmbed::default();
+    let vote_result_embed = {
+        vote_result_embed.custom_default(&record_id);
+
+        match status {
+            AgendaStatus::Approved => vote_result_embed.success_color(),
+            AgendaStatus::Declined => vote_result_embed.failure_color(),
+            _ => &mut vote_result_embed,
+        }
+        .title(format!(
+            "採決終了: {}は{}されました",
+            current_agenda.id.formatted(),
+            status.ja()
+        ))
+        .to_owned()
+    };
+
+    // ステータスに応じてRedmineとグローバル変数を更新
+    match status {
+        AgendaStatus::Approved => {
+            let _ = module.agenda_usecase().accept(current_agenda.id).await;
+            global::agendas::approve(current_agenda.id);
+        }
+        AgendaStatus::Declined => {
+            let _ = module.agenda_usecase().decline(current_agenda.id).await;
+            global::agendas::decline(current_agenda.id);
+        }
+        _ => {}
+    };
+
+    // 次の議題を選択
+    let next_agenda_id = global::agendas::find_next();
+    // 次の議題があれば、グローバル変数の中の議題のステータスを進行中に変更
+    if let Some(id) = next_agenda_id {
+        global::agendas::in_progress(id);
+    }
+    // 次の議題の存否に応じてEmbedを作成
+    let mut agenda_embed = CreateEmbed::default();
+    let agenda_embed = match next_agenda_id {
+        Some(id) => {
+            let next_agenda = module.agenda_usecase().find_new(id).await?;
+
+            println!("Next Agenda: #{}", next_agenda_id.unwrap().0);
+
+            discord_embeds::next_agenda_embed(&mut agenda_embed, &record_id, &next_agenda)
+        }
+        None => discord_embeds::no_next_agenda(&mut agenda_embed, &record_id),
+    }
+    .to_owned();
+
+    Ok(InteractionResponse::Embeds(vec![
+        vote_result_embed,
+        agenda_embed,
+    ]))
 }
