ec122782ce90a46c40653abf7c71c45773e87028
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-05-15 12:26:53 +0900

    feat(presentation): add get_voice_states method

--- crate-shared/src/discussion.rs
+++ crate-shared/src/discussion.rs
@@ -3,22 +3,31 @@ use crate_domain::error::MyError;
 use anyhow::anyhow;
 use serenity::{
     cache::Cache,
-    model::id::{ChannelId, GuildId, UserId},
+    model::{
+        id::{ChannelId, GuildId, UserId},
+        voice::VoiceState,
+    },
 };
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
+
+pub async fn get_voice_states(
+    cache: &Arc<Cache>,
+    guild_id: &GuildId,
+) -> anyhow::Result<HashMap<UserId, VoiceState>> {
+    cache
+        .guild(guild_id)
+        .await
+        .ok_or_else(|| anyhow!("guildが見つかりませんでした。（guild_id: {}）", guild_id))
+        .map(|guild| guild.voice_states)
+}
 
 pub async fn find_vc_by_user_id(
     cache: &Arc<Cache>,
     guild_id: &GuildId,
     user_id: &UserId,
 ) -> anyhow::Result<ChannelId> {
-    let guild = cache
-        .guild(guild_id)
-        .await
-        .ok_or_else(|| anyhow!("guildが見つかりませんでした。（guild_id: {}）", guild_id))?;
-
-    guild
-        .voice_states
+    get_voice_states(cache, guild_id)
+        .await?
         .get(user_id)
         .and_then(|state| state.channel_id)
         .ok_or_else(|| MyError::IsNotJoinedInVC.into())
