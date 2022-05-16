95d2f1a61d17360857844c597fee1741351a6d0c
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-05-16 19:48:51 +0900

    refactor(presentation): move codes

--- crate-presentation/src/command/create.rs
+++ crate-presentation/src/command/create.rs
@@ -103,89 +103,6 @@ pub fn builder() -> SlashCommandBuilder {
         .into()
 }
 
-pub async fn new_record((map, ctx, interaction): ExecutorArgs) -> CommandResult {
-    let module = global::module::get();
-
-    // 次回の会議の日付・時刻を取得
-    let date = {
-        let arg_strs = vec!["next_date_year", "next_date_month", "next_date_day"];
-        let mut args = Vec::new();
-        for str in arg_strs.into_iter() {
-            let arg: u16 = map
-                .get(str)
-                .cloned()
-                .ok_or_else(|| MyError::ArgIsNotFound(str.to_string()))?
-                .try_into()
-                .unwrap();
-            args.push(arg);
-        }
-
-        NaiveDate::from_ymd(args[0].into(), args[1].into(), args[2].into())
-    };
-    let start_time = {
-        let args: Vec<u16> = vec![("next_time_hour", 21), ("next_time_minute", 0)]
-            .into_iter()
-            .map(|(str, default)| {
-                map.get(str)
-                    .cloned()
-                    .map(|arg| arg.try_into().unwrap())
-                    .unwrap_or(default)
-            })
-            .collect_vec();
-
-        NaiveTime::from_hms(args[0].into(), args[1].into(), 0)
-    };
-    let end_time = start_time + Duration::hours(2);
-
-    // 次回の会議の日付・時刻を文字列にフォーマット
-    let date_str = format!("{}({})", date.format("%Y/%m/%d"), date.weekday_ja());
-    let time_formatter = "%H:%M";
-    let start_time_str = start_time.format(time_formatter);
-    let end_time_str = end_time.format(time_formatter);
-
-    ensure!(
-        Local::now().naive_local() <= NaiveDateTime::new(date, start_time),
-        format!(
-            "現在または現在より未来の日時を指定してください。: {} {}",
-            date_str, start_time_str
-        )
-    );
-
-    // 次回の会議の回数を取得
-    let latest_closed_record = module
-        .record_usecase()
-        .find_latest_closed()
-        .await
-        .context("No closed record")?;
-    let next_discussion_number = latest_closed_record
-        .discussion_number()
-        .context("Error while getting latest record number")?
-        + 1;
-
-    // 議事録のタイトルと説明文を生成
-    let record_title = format!("{}　第{}回アイデア会議", date_str, next_discussion_number);
-    let record_description_date_time =
-        format!("{}\n{}〜{}\n", date_str, start_time_str, end_time_str);
-    let record_description = format!("{}{}", record_description_date_time, RECORD_DESCRIPTIONS);
-
-    // 議事録をRedmine上に作成
-    let new_record_param = RecordParam::new(record_title, record_description, None, Some(date));
-    let new_record = module.record_usecase().add(new_record_param).await?;
-
-    let embed = CreateEmbed::default()
-        .title("議事録を新規作成しました")
-        .custom_field("タイトル", &new_record.title, false)
-        .custom_field("URL", new_record.url(), false)
-        .current_timestamp()
-        .success_color()
-        .to_owned();
-
-    interaction
-        .send(&ctx.http, InteractionResponse::Embed(embed))
-        .await
-        .map(|_| ())
-}
-
 #[allow(clippy::type_complexity)]
 pub async fn issue((map, ctx, interaction): ExecutorArgs) -> CommandResult {
     let module = global::module::get();
@@ -322,6 +239,89 @@ pub async fn issue((map, ctx, interaction): ExecutorArgs) -> CommandResult {
 
     interaction
         .send(&ctx.http, InteractionResponse::Embed(result_embed))
+        .await
+        .map(|_| ())
+}
+
+pub async fn new_record((map, ctx, interaction): ExecutorArgs) -> CommandResult {
+    let module = global::module::get();
+
+    // 次回の会議の日付・時刻を取得
+    let date = {
+        let arg_strs = vec!["next_date_year", "next_date_month", "next_date_day"];
+        let mut args = Vec::new();
+        for str in arg_strs.into_iter() {
+            let arg: u16 = map
+                .get(str)
+                .cloned()
+                .ok_or_else(|| MyError::ArgIsNotFound(str.to_string()))?
+                .try_into()
+                .unwrap();
+            args.push(arg);
+        }
+
+        NaiveDate::from_ymd(args[0].into(), args[1].into(), args[2].into())
+    };
+    let start_time = {
+        let args: Vec<u16> = vec![("next_time_hour", 21), ("next_time_minute", 0)]
+            .into_iter()
+            .map(|(str, default)| {
+                map.get(str)
+                    .cloned()
+                    .map(|arg| arg.try_into().unwrap())
+                    .unwrap_or(default)
+            })
+            .collect_vec();
+
+        NaiveTime::from_hms(args[0].into(), args[1].into(), 0)
+    };
+    let end_time = start_time + Duration::hours(2);
+
+    // 次回の会議の日付・時刻を文字列にフォーマット
+    let date_str = format!("{}({})", date.format("%Y/%m/%d"), date.weekday_ja());
+    let time_formatter = "%H:%M";
+    let start_time_str = start_time.format(time_formatter);
+    let end_time_str = end_time.format(time_formatter);
+
+    ensure!(
+        Local::now().naive_local() <= NaiveDateTime::new(date, start_time),
+        format!(
+            "現在または現在より未来の日時を指定してください。: {} {}",
+            date_str, start_time_str
+        )
+    );
+
+    // 次回の会議の回数を取得
+    let latest_closed_record = module
+        .record_usecase()
+        .find_latest_closed()
+        .await
+        .context("No closed record")?;
+    let next_discussion_number = latest_closed_record
+        .discussion_number()
+        .context("Error while getting latest record number")?
+        + 1;
+
+    // 議事録のタイトルと説明文を生成
+    let record_title = format!("{}　第{}回アイデア会議", date_str, next_discussion_number);
+    let record_description_date_time =
+        format!("{}\n{}〜{}\n", date_str, start_time_str, end_time_str);
+    let record_description = format!("{}{}", record_description_date_time, RECORD_DESCRIPTIONS);
+
+    // 議事録をRedmine上に作成
+    let new_record_param = RecordParam::new(record_title, record_description, None, Some(date));
+    let new_record = module.record_usecase().add(new_record_param).await?;
+
+    let embed = CreateEmbed::default()
+        .title("議事録を新規作成しました")
+        .custom_field("タイトル", &new_record.title, false)
+        .custom_field("URL", new_record.url(), false)
+        .current_timestamp()
+        .success_color()
+        .to_owned();
+
+    interaction
+        .send(&ctx.http, InteractionResponse::Embed(embed))
         .await
         .map(|_| ())
 }
