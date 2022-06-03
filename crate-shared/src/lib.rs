d7b94a3c438d9d7ecc839d52d9349a76ca7ae96d
Lucky3028 <lucky3028@users.noreply.github.com>
2022-06-03 11:06:59 +0900

    fix(shared): remove unnecessary code

--- crate-shared/src/lib.rs
+++ crate-shared/src/lib.rs
@@ -1,7 +1,5 @@
 mod env;
 pub use env::Env;
 
-pub use serenity::client::Context as SerenityContext;
-
 pub const REDMINE_URL: &str = "https://redmine.seichi.click";
 pub const GITHUB_URL: &str = "https://api.github.com/repos/GiganticMinecraft/SeichiAssist/issues";
