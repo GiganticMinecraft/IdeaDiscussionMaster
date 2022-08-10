11b48f3fef42f6dde2b6924e09e52511162d8a8e
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-10 14:39:02 +0900

    test: add tests of status

--- c-domain/src/status.rs
+++ c-domain/src/status.rs
@@ -47,3 +47,56 @@ impl Default for RecordStatus {
         Self::New
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use strum::IntoEnumIterator;
+    use test_case::test_case;
+
+    #[test_case(AgendaStatus::New => true; "new")]
+    #[test_case(AgendaStatus::InProgress => false; "in_progress")]
+    #[test_case(AgendaStatus::Declined => false; "declined")]
+    #[test_case(AgendaStatus::Approved => false; "approved")]
+    fn agenda_status_is_new(status: AgendaStatus) -> bool {
+        status.is_new()
+    }
+
+    #[test]
+    fn default_agenda_status() {
+        assert_eq!(AgendaStatus::default(), AgendaStatus::New);
+    }
+
+    #[test_case("id"; "id")]
+    #[test_case("ja"; "ja")]
+    #[test_case("emoji"; "emoji")]
+    fn all_agenda_status_props_is_set(props: &str) {
+        assert!(AgendaStatus::iter()
+            .map(|s| s.get_str(props))
+            .all(|opt| opt.is_some()))
+    }
+
+    #[test_case(RecordStatus::New => true; "new")]
+    #[test_case(RecordStatus::Closed => false; "closed")]
+    fn record_status_is_new(status: RecordStatus) -> bool {
+        status.is_new()
+    }
+
+    #[test_case(RecordStatus::New => false; "new")]
+    #[test_case(RecordStatus::Closed => true; "closed")]
+    fn record_status_is_closed(status: RecordStatus) -> bool {
+        status.is_closed()
+    }
+
+    #[test]
+    fn default_record_status() {
+        assert_eq!(RecordStatus::default(), RecordStatus::New);
+    }
+
+    #[test_case("id"; "id")]
+    fn all_record_status_props_is_set(props: &str) {
+        assert!(RecordStatus::iter()
+            .map(|s| s.get_str(props))
+            .all(|opt| opt.is_some()))
+    }
+}
