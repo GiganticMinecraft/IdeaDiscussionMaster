0b17124db69e859d9c4398e0e1415a56318c0e0a
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-11 19:59:02 +0900

    refactor(infra: init repo impl with url only

--- c-infra/src/repository.rs
+++ c-infra/src/repository.rs
@@ -1,8 +1,19 @@
 mod agenda;
 mod record;
 
-#[derive(derive_new::new)]
+use crate::client::RedmineClient;
+use std::marker::PhantomData;
+
 pub struct RedmineRepositoryImpl<T> {
-    pub client: crate::client::RedmineClient,
+    pub client: RedmineClient,
     marker: std::marker::PhantomData<T>,
+}
+
+impl<T> RedmineRepositoryImpl<T> {
+    pub fn new(url: String) -> Self {
+        Self {
+            client: RedmineClient::new(url),
+            marker: PhantomData::default(),
+        }
+    }
 }
