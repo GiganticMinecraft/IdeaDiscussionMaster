0722372bb1af848e5bd24efada34c8de41f85f0b
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-11 16:13:22 +0900

    test(usecase): add repo tests

---
+++ c-usecase/tests/agenda.rs
@@ -0,0 +1,41 @@
+mod agenda_fixture;
+use agenda_fixture::AgendaFixture;
+
+use c_domain::{id::AgendaId, repository::MockAgendaRepository, Agenda};
+use c_usecase::AgendaUseCase;
+
+use std::sync::Arc;
+
+#[tokio::test]
+async fn test_with_err_repo() {
+    let mut repo = MockAgendaRepository::new();
+    repo.expect_find()
+        .returning(|_| Err(anyhow::anyhow!("There are no agendas")));
+    let use_case = AgendaUseCase::new(Arc::new(repo));
+
+    assert!(use_case.find(AgendaId::new(1)).await.is_err());
+    assert!(use_case.find(AgendaId::new(2)).await.is_err());
+    assert!(use_case.find(AgendaId::new(3)).await.is_err());
+}
+
+#[tokio::test]
+async fn test_with_fixtures() {
+    let mut repo = MockAgendaRepository::new();
+    repo.expect_find().returning(|id| {
+        Agenda::all_fixtures()
+            .into_iter()
+            .find(|agenda| agenda.id == id)
+            .ok_or(anyhow::anyhow!("The agenda you want does not exist"))
+    });
+    repo.expect_save().returning(|_| Ok(()));
+    let use_case = AgendaUseCase::new(Arc::new(repo));
+
+    assert!(use_case.find(AgendaId::new(1)).await.is_ok());
+    assert!(use_case.find(AgendaId::new(2)).await.is_ok());
+    assert!(use_case.find(AgendaId::new(10)).await.is_err());
+
+    assert!(use_case.find_new(AgendaId::new(1)).await.is_ok());
+    assert!(use_case.find_new(AgendaId::new(2)).await.is_err());
+    assert!(use_case.find_new(AgendaId::new(3)).await.is_err());
+    assert!(use_case.find_new(AgendaId::new(10)).await.is_err());
+}
---
+++ c-usecase/tests/record.rs
@@ -0,0 +1,78 @@
+mod record_fixture;
+use record_fixture::RecordFixture;
+
+use c_domain::{id::RecordId, repository::MockRecordRepository, status::RecordStatus, Record};
+use c_usecase::RecordUseCase;
+
+use std::sync::Arc;
+
+#[tokio::test]
+async fn test_with_err_repo() {
+    let mut repo = MockRecordRepository::new();
+    repo.expect_find()
+        .returning(|_| Err(anyhow::anyhow!("There are no records")));
+    let use_case = RecordUseCase::new(Arc::new(repo));
+
+    assert!(use_case.find(RecordId::new(1)).await.is_err());
+    assert!(use_case.find(RecordId::new(2)).await.is_err());
+    assert!(use_case.find(RecordId::new(3)).await.is_err());
+}
+
+#[tokio::test]
+async fn test_with_fixtures() {
+    let mut repo = MockRecordRepository::new();
+    repo.expect_find().returning(|id| {
+        Record::all_fixtures()
+            .into_iter()
+            .find(|record| record.id == id)
+            .ok_or(anyhow::anyhow!("The record you want does not exist"))
+    });
+    repo.expect_list()
+        .withf(|_, statuses| !statuses.is_empty())
+        .returning(|limit, statuses| {
+            Ok(Record::all_fixtures()
+                .into_iter()
+                .filter(|record| statuses.contains(&record.status))
+                .take(limit.unwrap_or(20).into())
+                .collect())
+        });
+    let use_case = RecordUseCase::new(Arc::new(repo));
+
+    assert!(use_case.find(RecordId::new(1)).await.is_ok());
+    assert!(use_case.find(RecordId::new(2)).await.is_ok());
+    assert!(use_case.find(RecordId::new(10)).await.is_err());
+
+    assert!(use_case.find_new(RecordId::new(1)).await.is_ok());
+    assert!(use_case.find_new(RecordId::new(2)).await.is_err());
+    assert!(use_case.find_new(RecordId::new(3)).await.is_err());
+    assert!(use_case.find_new(RecordId::new(10)).await.is_err());
+
+    assert_eq!(
+        use_case.list(None, vec![RecordStatus::New]).await.unwrap(),
+        vec![Record::new1().into()]
+    );
+    assert_eq!(
+        use_case
+            .list(None, vec![RecordStatus::New, RecordStatus::Closed])
+            .await
+            .unwrap(),
+        vec![
+            Record::new1().into(),
+            Record::closed1().into(),
+            Record::closed2().into(),
+            Record::closed3().into()
+        ]
+    );
+    assert_eq!(
+        use_case
+            .list(Some(2), vec![RecordStatus::New, RecordStatus::Closed])
+            .await
+            .unwrap(),
+        vec![Record::new1().into(), Record::closed1().into()]
+    );
+
+    assert_eq!(
+        use_case.find_latest_closed().await.unwrap(),
+        Record::closed1().into()
+    );
+}
