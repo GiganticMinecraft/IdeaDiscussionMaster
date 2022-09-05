549d43a7719d8c53682f7abe336696f804a7d4d3
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-09-05 17:51:43 +0900

    feat(presentation): add register command

---
+++ c-presentation/src/commands/register.rs
@@ -0,0 +1,9 @@
+use super::{Context, Error};
+
+/// Shows buttons to add or remove slash commands. (prefix command)
+#[poise::command(prefix_command)]
+pub async fn register(ctx: Context<'_>) -> Result<(), Error> {
+    poise::builtins::register_application_commands_buttons(ctx).await?;
+
+    Ok(())
+}
