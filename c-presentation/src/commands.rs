c47f756b3007deadc35cdd9a5da02397930622c3
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-09-05 18:34:07 +0900

    feat(presentation): add CommandResult type alias

--- c-presentation/src/commands.rs
+++ c-presentation/src/commands.rs
@@ -2,4 +2,5 @@ mod register;
 pub use register::register;
 
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type CommandResult = Result<(), Error>;
 pub type Context<'a> = poise::Context<'a, crate::shared::Data, Error>;
--- c-presentation/src/commands/register.rs
+++ c-presentation/src/commands/register.rs
@@ -1,8 +1,8 @@
-use super::{Context, Error};
+use crate::commands::{CommandResult, Context};
 
 /// Shows buttons to add or remove slash commands. (prefix command)
 #[poise::command(prefix_command)]
-pub async fn register(ctx: Context<'_>) -> Result<(), Error> {
+pub async fn register(ctx: Context<'_>) -> CommandResult {
     poise::builtins::register_application_commands_buttons(ctx).await?;
 
     Ok(())
