d274117ec7873a3504f5e1a4a65e53c8b95a42ca
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-09-05 20:24:26 +0900

    feat(usecase): add AgendaUseCase#in_progress

--- c-usecase/src/agenda.rs
+++ c-usecase/src/agenda.rs
@@ -26,6 +26,13 @@ impl AgendaUseCase {
         Ok(issue)
     }
 
+    pub async fn in_progress(&self, id: AgendaId) -> anyhow::Result<()> {
+        let agenda = self.repo.find(id).await?;
+        let agenda = agenda.in_progress();
+
+        self.repo.save(agenda).await
+    }
+
     pub async fn approve(&self, id: AgendaId) -> anyhow::Result<()> {
         let agenda = self.repo.find(id).await?;
         let agenda = agenda.approve();
