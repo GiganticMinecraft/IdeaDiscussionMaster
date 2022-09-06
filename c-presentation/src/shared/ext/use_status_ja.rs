4d522a59fe33e3cf1a24e3a5ceb25890753e2e73
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-09-06 15:07:55 +0900

    feat(presentation): add UseStatusJa trait

---
+++ c-presentation/src/shared/ext/use_status_ja.rs
@@ -0,0 +1,16 @@
+use c_domain::status::{AgendaStatus, RecordStatus};
+
+use strum::EnumProperty;
+
+pub trait UseStatusJa {
+    fn ja(&self) -> String
+    where
+        Self: EnumProperty,
+    {
+        self.get_str("id").unwrap().to_string()
+    }
+}
+
+impl UseStatusJa for AgendaStatus {}
+
+impl UseStatusJa for RecordStatus {}
