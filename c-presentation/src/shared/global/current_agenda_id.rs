0cf472a9452d35b35159d759b3fd19658ccca964
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-09-11 17:01:09 +0900

    revert(presentation): impl GlobalCurrentAgendaId

---
+++ c-presentation/src/shared/global/current_agenda_id.rs
@@ -0,0 +1,22 @@
+use derive_new::new;
+use once_cell::sync::Lazy;
+use std::sync::{Arc, Mutex};
+
+#[derive(new)]
+pub struct GlobalCurrentAgendaId(#[new(default)] Lazy<Arc<Mutex<Option<u16>>>>);
+
+impl GlobalCurrentAgendaId {
+    pub fn save(&self, id: u16) {
+        let mut lock = self.0.lock().unwrap();
+        *lock = Some(id)
+    }
+
+    pub fn clear(&self) {
+        let mut lock = self.0.lock().unwrap();
+        *lock = None
+    }
+
+    pub fn get(&self) -> Option<u16> {
+        *self.0.lock().unwrap()
+    }
+}
