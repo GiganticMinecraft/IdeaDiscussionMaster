1ce232a78964eba2d80f7f2ffac3f9a8748b200c
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-09-13 13:50:23 +0900

    feat(presentation): add CommandError#AgendaIsNotFound

--- c-presentation/src/shared/command_error.rs
+++ c-presentation/src/shared/command_error.rs
@@ -4,4 +4,6 @@ use thiserror::Error;
 pub enum CommandError {
     #[error("このコマンドを実行するには会議が開始されている必要があります")]
     DiscussionHasBeenStarted,
+    #[error("現在進行中の議題はありません")]
+    AgendaIsNotFound,
 }
