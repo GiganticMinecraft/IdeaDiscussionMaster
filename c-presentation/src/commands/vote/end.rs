841327e2051789371750f1a4c1b19e17bf81356f
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-09-16 20:54:24 +0900

    feat(presentation): impl `/vote end`

--- c-presentation/src/commands/vote/end.rs
+++ c-presentation/src/commands/vote/end.rs
@@ -1,18 +1,44 @@
-use crate::{
-    commands::{CommandResult, Context},
-    shared::CommandError,
-};
-use c_domain::id::RecordId;
+use crate::commands::{CommandResult, Context};
+use c_domain::status::AgendaStatus;
+
+use crate::commands::vote::shared::end_votes;
+use anyhow::anyhow;
+use itertools::Itertools;
+use log::info;
+use poise::futures_util::{future, stream, Stream, StreamExt};
+
+async fn autocomplete_agenda_status_string<'a>(
+    _ctx: Context<'_>,
+    partial: &'a str,
+) -> impl Stream<Item = String> + 'a {
+    stream::iter(
+        AgendaStatus::closed()
+            .into_iter()
+            .map(|status| status.to_string()),
+    )
+    .filter(move |name| future::ready(name.to_lowercase().starts_with(&partial.to_lowercase())))
+}
 
 /// 採決を終了します
 #[poise::command(slash_command)]
-pub async fn end(ctx: Context<'_>) -> CommandResult {
-    let _record_id = ctx
-        .data()
-        .record_id
-        .get()
-        .map(RecordId::new)
-        .ok_or(CommandError::DiscussionHasBeenStarted)?;
+pub async fn end(
+    ctx: Context<'_>,
+    #[description = "変更後のステータス"]
+    #[autocomplete = "autocomplete_agenda_status_string"]
+    status: String,
+) -> CommandResult {
+    let status = AgendaStatus::from_string(&status).ok_or_else(|| {
+        anyhow!(
+            "議題のステータスは{}のうちいずれか1つのみ指定できます",
+            AgendaStatus::closed()
+                .iter()
+                .map(|status| format!("「{:?}」", status))
+                .collect_vec()
+                .join("、")
+        )
+    })?;
+    info!("Vote finished: {:?}", status);
+    end_votes(&ctx, status).await?;
 
     Ok(())
 }
