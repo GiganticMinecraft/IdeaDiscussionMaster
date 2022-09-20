25193752b0236a99e61b832a3a741098b3f2a501
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-09-20 13:33:35 +0900

    fix(presentation): sort agendas by id when grouping

--- c-presentation/src/shared/ext/sort_agendas.rs
+++ c-presentation/src/shared/ext/sort_agendas.rs
@@ -11,7 +11,8 @@ pub trait SortAgendasExt {
 impl SortAgendasExt for Vec<AgendaDto> {
     fn sort_and_grouping_by_status(&self) -> Vec<(AgendaStatus, Vec<AgendaDto>)> {
         // ソート
-        let agendas = self
+        let agendas = self.sort_by_id();
+        let agendas = agendas
             .iter()
             .sorted_by_cached_key(|agenda| agenda.status)
             .collect_vec();
