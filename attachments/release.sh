39fb55f535ec6d7df1eba86135e65180dcba30ce
Lucky <Lucky3028@users.noreply.github.com>
2022-10-25 12:04:08 +0900

    chore: remove untagged images on release [skip ci]

--- attachments/release.sh
+++ attachments/release.sh
@@ -3,3 +3,6 @@
 docker-compose down
 docker-compose pull
 docker-compose up -d
+# Remove untagged images
+# see: https://stackoverflow.com/questions/33913020/docker-remove-none-tag-images
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
