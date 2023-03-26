62424866022ccd28d9fda75b2c56726bb8300cfb
Lucky <Lucky3028@users.noreply.github.com>
2023-03-26 22:02:07 +0900

    chore(release.sh): replace docker-compose with "docker compose"

--- attachments/release.sh
+++ attachments/release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker-compose down
-docker-compose pull
-docker-compose up -d
+docker compose down
+docker compose pull
+docker compose up -d
 # Remove untagged images
 # see: https://stackoverflow.com/questions/33913020/docker-remove-none-tag-images
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc) || :
